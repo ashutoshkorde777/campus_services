bb54085f4efebada734fec017b72734221a2bc11
Shreyas kempwade <124675717+Shreyas1427@users.noreply.github.com>
2025-01-30 12:17:48 +0530

    Inventory

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("./models/User.js");
+const User = require("../models/User.js");
 
 const getAllUsers = async (req, res) => {
     try {
---
+++ backend/src/routes/serviceProviderRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const { getServices, addService, updateService } = require('../controllers/serviceProviderController');
+
+// Routes
+router.get('/services',protect, getServices); // Get all services for the service provider
+router.post('/services',protect,addService); // Add a new service
+router.put('/services',protect, updateService); // Update an existing service
+
+module.exports = router;
