181d258fea6d5353c42219c7542e6e46cfb5f751
Ashutosh Korde <ashutoshkorde777@gmail.com>
2025-01-30 14:16:58 +0530

    some changes

--- backend/src/models/service.js
+++ backend/src/models/service.js
@@ -8,6 +8,7 @@ const serviceSchema = new mongoose.Schema({
   providerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Linked to the service provider
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  image: { type: String, required: true }, // URL or path to the image
 });
 
 module.exports = mongoose.model('Service', serviceSchema);
